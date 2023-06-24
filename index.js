853c736334709133b3781986ec53b0870655259a
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-06-24 15:55:51 +0600

    project setup with swagger file

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./swagger.yaml');
+
+//express app
+const app = express();
+app.use(express.json());
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+app.get('/health', (_req, res) => {
+  res.status(200).json({
+    health: 'OK',
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
