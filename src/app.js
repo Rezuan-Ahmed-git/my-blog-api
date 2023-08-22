9c22d6f66b02cc45d1d8a824f1eb5a48af5dc464
rezuan <rezuan.ahmed.pro@gmail.com>
2023-08-22 15:29:18 +0600

    feat: database setup with docker

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const YAML = require('yamljs');
 const swaggerDoc = YAML.load('./swagger.yaml');
 const OpenApiValidator = require('express-openapi-validator');
 
-// const User = require('./model/User');
+const User = require('./model/User');
 
 //express app
 const app = express();
@@ -43,18 +43,24 @@ app.use((err, req, res, next) => {
 let connectionURL = process.env.DB_CONNECTION_URL;
 connectionURL = connectionURL.replace('<username>', process.env.DB_USERNAME);
 connectionURL = connectionURL.replace('<password>', process.env.DB_PASSWORD);
-connectionURL = `${connectionURL}/${process.env.DB_NAME}?${process.env.DB_URL_QUERY}`;
+// connectionURL = `${connectionURL}/${process.env.DB_NAME}?${process.env.DB_URL_QUERY}`;
 
 mongoose
-  .connect(connectionURL, {
-    serverSelectionTimeoutMS: 500,
-  })
+  .connect(connectionURL, { dbName: process.env.DB_NAME })
   .then(() => {
     console.log('Database connected');
     app.listen(4000, async () => {
       console.log('Server is running on port 4000');
       // const users = await User.find({});
       // console.log(users);
+      const user = new User({
+        name: 'Rezuan Ahmed',
+        email: 'rezuan@gmail.com',
+        password: '123456',
+      });
+
+      await user.save();
+      console.log('User created');
     });
   })
   .catch((e) => {
