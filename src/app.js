1b1f48f299e5c1f2a6571bb34f82c57fdda2cdfe
rezuan <rezuan.ahmed.pro@gmail.com>
2023-08-22 16:30:35 +0600

    refact: perfectly refactor and setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
-
 const applyMiddleware = require('./middleware/index');
 
 //express app
@@ -24,20 +21,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-let connectionURL = process.env.DB_CONNECTION_URL;
-connectionURL = connectionURL.replace('<username>', process.env.DB_USERNAME);
-connectionURL = connectionURL.replace('<password>', process.env.DB_PASSWORD);
-// connectionURL = `${connectionURL}/${process.env.DB_NAME}?${process.env.DB_URL_QUERY}`;
-
-mongoose
-  .connect(connectionURL, { dbName: process.env.DB_NAME })
-  .then(() => {
-    console.log('Database connected');
-    app.listen(4000, async () => {
-      console.log('Server is running on port 4000');
-    });
-  })
-  .catch((e) => {
-    console.log('Database connection failed');
-    console.log('Message: ', e.message);
-  });
+module.exports = app;
